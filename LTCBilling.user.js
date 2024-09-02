ae0708ca134f05015faec5a98410a6c7b9c5c581
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2024-09-01 21:02:10 -0700

    Create LTCBilling.user.js

---
+++ LTCBilling.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        LTC Billing
+// @namespace   https://github.com/maywoodmedical/Oscar
+// @description Automatically select LTC billing form and service location 
+// @include     *billing.do?billRegion=BC&billForm=GP&hotclick=*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
+// @updateURL 
+// @downloadURL 
+// @version 1.0
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Function to select the option from a dropdown
+    function selectOptionByText(dropdownId, optionText) {
+        let dropdown = document.getElementById(dropdownId);
+        if (dropdown) {
+            for (let i = 0; i < dropdown.options.length; i++) {
+                if (dropdown.options[i].text === optionText) {
+                    dropdown.selectedIndex = i;
+                    dropdown.dispatchEvent(new Event('change')); // Trigger change event if needed
+                    break;
+                }
+            }
+        }
+    }
+
+    // Function to tick the checkbox
+    function tickCheckboxByValue(checkboxValue) {
+        let checkboxes = document.querySelectorAll(`input[type="checkbox"][value="${checkboxValue}"]`);
+        checkboxes.forEach(checkbox => {
+            if (!checkbox.checked) {
+                checkbox.checked = true;
+                checkbox.dispatchEvent(new Event('change')); // Trigger change event if needed
+            }
+        });
+    }
+
+    // Function to fill the text box
+    function fillTextBoxByName(inputName, value) {
+        let textBox = document.querySelector(`input[type="text"][name="${inputName}"]`);
+        if (textBox) {
+            textBox.value = value;
+            textBox.dispatchEvent(new Event('input')); // Trigger input event if needed
+        }
+    }
+
+    // Function to set up the page
+    function setupPage() {
+        selectOptionByText('selectBillingForm', 'Long Term Care');
+        selectOptionByText('xml_visittype', 'Continuing Care facility');
+        tickCheckboxByValue('00114');
+        fillTextBoxByName('xml_other1', '00114');
+    }
+
+    // Run the function when the page is fully loaded
+    window.addEventListener('load', setupPage);
+})();

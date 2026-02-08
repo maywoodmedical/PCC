21a75f3a7338534847f22b2d7a753ab919746f24
maywoodmedical <115953271+maywoodmedical@users.noreply.github.com>
2026-02-08 11:59:11 -0800

    Update PCCDefaultProgressNote.user.js

--- PCCDefaultProgressNote.user.js
+++ PCCDefaultProgressNote.user.js
@@ -1,65 +1,61 @@
 // ==UserScript==
-// @name           PCC Default Progress Note
-// @namespace      https://github.com/maywoodmedical/Oscar
-// @description    Sets the default for Point Click Care progress note settings
-// @include        *chart/ipn/newipn.jsp*
-// @require   http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js
-// @updateURL https://github.com/maywoodmedical/PCC/raw/main/DefaultProgressNote.user.js
-// @downloadURL https://github.com/maywoodmedical/PCC/raw/main/DefaultProgressNote.user.js
-// @version 1.0
+// @name          PCC Default Progress Note (Fast & Responsive)
+// @namespace     https://github.com/maywoodmedical/Oscar
+// @description   Uses a fast interval to apply settings as soon as elements appear
+// @include       *chart/ipn/newipn.jsp*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+// @version       1.7
 // ==/UserScript==
 
-//========Get Path============
-var elements = (window.location.pathname.split('/', 2))
-firstElement = (elements.slice(1))
-vPath = ('https://' + location.host + '/' + firstElement + '/') //=====Get Parameters============
-var params = {};
-if (location.search) {
-    var parts = location.search.substring(1).split('&');
-    for (var i = 0; i < parts.length; i++) {
-        var nv = parts[i].split('=');
-        if (!nv[0]) continue;
-        params[nv[0]] = nv[1] || true;
-    }
-} //alert(params.docType)
+(function() {
+    'use strict';
 
-// Choose Physician Visit in the drop down visit type
-var theDefault = 'Physician Visit';
-var theOptions = document.getElementsByName('pn_type_id')[0].options;
-for (var theOption of theOptions) {
-    if (typeof(theOption) == 'object') {
-        if (theOption.text == theDefault) {
-            theOption.selected = true;
-            break;
+    let hasRun = false;
+
+    function getFormattedFriday() {
+        let d = new Date();
+        let day = d.getDay();
+        let diff = (day + 7 - 5) % 7; 
+        d.setDate(d.getDate() - diff);
+        return (d.getMonth() + 1).toString().padStart(2, '0') + '/' +
+               d.getDate().toString().padStart(2, '0') + '/' +
+               d.getFullYear();
+    }
+
+    // This function will run repeatedly until it succeeds once
+    const checkExist = setInterval(function() {
+        const $dropdown = $('select[name="pn_type_id"]');
+        const $dateField = $('input[name="effective_date_dummy"]');
+        const $hiddenField = $('input[name="effective_date"]');
+
+        // Check if the specific fields we need are rendered
+        if ($dropdown.length && $dateField.length && !hasRun) {
+            
+            const friday = getFormattedFriday();
+
+            // 1. Set Dropdown
+            $dropdown.val($dropdown.find('option:contains("Physician Visit")').val());
+
+            // 2. Set Date Fields
+            $dateField.val(friday);
+            if ($hiddenField.length) {
+                $hiddenField.val(friday);
+            }
+
+            // 3. Trigger events to lock the value in the site's memory
+            $dateField[0].dispatchEvent(new Event('input', { bubbles: true }));
+            $dateField[0].dispatchEvent(new Event('change', { bubbles: true }));
+            $dateField[0].dispatchEvent(new Event('blur', { bubbles: true }));
+
+            console.log("PCC Fast-Load: Success.");
+            
+            // 4. Clean up: Stop the interval and lock the script
+            hasRun = true;
+            clearInterval(checkExist); 
         }
-    }
-}
+    }, 100); // Check every 100ms (0.1 seconds)
 
+    // Safety: If for some reason the elements never appear, stop checking after 10 seconds
+    setTimeout(() => clearInterval(checkExist), 10000);
 
-/// Function to get the date of the previous Friday
-function getPreviousFridayDate() {
-    var today = new Date();
-    var day = today.getDay();
-    var daysToFriday = (day + 7 - 5) % 7; // Calculate how many days to go back to reach Friday
-    var previousFriday = new Date(today);
-    previousFriday.setDate(today.getDate() - daysToFriday); // Set the date to the previous Friday
-
-    return previousFriday;
-}
-
-// Function to format the date as 'MM/DD/YYYY'
-function formatDate(date) {
-    var month = String(date.getMonth() + 1).padStart(2, '0');
-    var day = String(date.getDate()).padStart(2, '0');
-    var year = date.getFullYear();
-    return month + '/' + day + '/' + year;
-}
-
-// Get the previous Friday date
-var previousFridayDate = getPreviousFridayDate();
-
-// Format the date
-var formattedDate = formatDate(previousFridayDate);
-
-// Set the value of the text box 'effective_date_dummy' to the previous Friday date
-document.getElementsByName('effective_date_dummy')[0].value = formattedDate;
+})();
